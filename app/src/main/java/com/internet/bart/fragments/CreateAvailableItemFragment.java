605834001ddfd5aed19d55bd48d7cd4153c4a101
Ken Luk <ken.luk@detroitlabs.com>
2014-12-06 19:29:25 -0500

    Listing creation can take picture and display image preview

--- app/src/main/java/com/internet/bart/fragments/CreateAvailableItemFragment.java
+++ app/src/main/java/com/internet/bart/fragments/CreateAvailableItemFragment.java
@@ -1,20 +1,26 @@
 package com.internet.bart.fragments;
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.internet.bart.R;
-import com.internet.bart.adapters.AvailableItemsAdapter;
-import com.internet.bart.apis.ParseRestApi;
 
 /**
  * Created on 12/5/14.
  */
-public class CreateAvailableItemFragment extends Fragment {
+public class CreateAvailableItemFragment extends Fragment implements View.OnClickListener {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private Button addPhotoButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -22,5 +28,36 @@ public class CreateAvailableItemFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_create_available_item, container ,false);
 
         return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        addPhotoButton = (Button) getActivity().findViewById(R.id.add_photo_button);
+        addPhotoButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.add_photo_button:
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+                break;
+            
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView previewImageView = (ImageView) getActivity().findViewById(R.id.preview_image_view);
+            previewImageView.setImageBitmap(imageBitmap);
+        }
     }
 }
