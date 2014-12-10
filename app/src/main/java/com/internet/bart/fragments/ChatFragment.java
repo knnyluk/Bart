4cc57e73ba58bb1b4292c16a4bf06f1e1636f2ed
Ken Luk <ken.luk@detroitlabs.com>
2014-12-10 12:57:51 -0500

    chat autorefresh working

--- app/src/main/java/com/internet/bart/fragments/ChatFragment.java
+++ app/src/main/java/com/internet/bart/fragments/ChatFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.internet.bart.R;
 import com.internet.bart.adapters.ChatMessageAdapter;
@@ -17,6 +18,9 @@ import com.internet.bart.models.ChatMessage;
 import com.internet.bart.models.TradeProposal;
 
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created on 12/9/14.
@@ -27,6 +31,9 @@ public class ChatFragment extends Fragment implements View.OnClickListener, Pars
     private ListView chatMessageListView;
     private EditText textEditText;
     private String tradeId;
+
+    private Timer refreshTimer;
+    private static final long REFRESH_INTERVAL = TimeUnit.SECONDS.toMillis(3);
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -51,7 +58,6 @@ public class ChatFragment extends Fragment implements View.OnClickListener, Pars
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.send_button:
-//                System.out.println("send button clicked");
                 String text = textEditText.getText().toString();
                 ChatMessage newMessage = new ChatMessage(text);
                 chatMessageAdapter.add(newMessage);
@@ -67,7 +73,43 @@ public class ChatFragment extends Fragment implements View.OnClickListener, Pars
         chatMessageAdapter = new ChatMessageAdapter(getActivity());
         chatMessageListView = (ListView) getActivity().findViewById(R.id.message_list_view);
         chatMessageListView.setAdapter(chatMessageAdapter);
-        ParseRestApi.getParseRestApi().getChatMessages(tradeId, this);
+        startRefreshTimer();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        stopRefreshTimer();
+    }
+
+    private void startRefreshTimer() {
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                autoRefreshList();
+            }
+        };
+
+        refreshTimer = new Timer();
+        refreshTimer.schedule(timerTask, 0, REFRESH_INTERVAL);
+    }
+
+    private void stopRefreshTimer() {
+        refreshTimer.cancel();
+        refreshTimer = null;
+    }
+
+    private void autoRefreshList() {
+        if (isAdded()) {
+
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+//                    Toast.makeText(getActivity(), "Auto refreshing messages", Toast.LENGTH_SHORT).show();
+                }
+            });
+            loadChatMessages();
+        }
     }
 
     @Override
@@ -83,5 +125,13 @@ public class ChatFragment extends Fragment implements View.OnClickListener, Pars
     @Override
     public void onError() {
 
+    }
+
+    private void loadChatMessages() {
+        if (tradeId != null) {
+            ParseRestApi.getParseRestApi().getChatMessages(tradeId, this);
+        } else {
+            throw new IllegalStateException("Lost track of the trade");
+        }
     }
 }
